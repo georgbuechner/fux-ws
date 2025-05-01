cab568f8da40a7be0cfc0fec0fb2a9f6d42b971b
fux <jan.dick@bdp.org>
2025-05-02 01:24:35 +0200

    Adds content

--- web/js/main.js
+++ web/js/main.js
@@ -19,25 +19,36 @@ async function Move() {
   }
 }
 
-function full(elem) {
-  var childDivs = document.getElementById('container').getElementsByTagName('div');
-  for ( i=0; i< childDivs.length; i++ ) {
-    if (!elem.classList.contains("full")) {
-      let ran = Math.floor(Math.random() * childDivs.length); 
-      childDivs[ran].classList.add("shrink");
-      ran = Math.floor(Math.random() * childDivs.length); 
-      childDivs[ran].classList.add("shrink");
-      ran = Math.floor(Math.random() * childDivs.length); 
-      childDivs[ran].classList.add("shrink");
-    } else {
-      childDivs[i].classList.remove("shrink");
-    }
-  }
-  elem.classList.remove("shrink");
+function full(card) {
+  var cards = document.querySelectorAll('.card');
 
-  elem.classList.toggle("full");
-  if (elem.classList.contains("full")) {
-    const rect = elem.getBoundingClientRect();
+  if (card.classList.contains('full')) {
+    // Reset card
+    card.classList.remove('full'); 
+    card.style.transform = '';
+
+    if (card.id !== '') {
+      card.classList.add('card-hidden');
+    }
+
+    // Reform all other cards:
+    cards.forEach(c => {
+      if (c !== card) {
+        c.classList.remove("shatter");
+        c.classList.add("reform");
+
+        setTimeout(() => c.classList.remove('reform'), 600); 
+      }
+    });
+  } else {
+    const targetId = card.dataset.target;
+    if (targetId !== undefined) {
+      card = document.getElementById(targetId); 
+      card.classList.remove('card-hidden');
+    }
+
+    // Expand card 
+    const rect = card.getBoundingClientRect();
     const cardCenterX = rect.left + rect.width / 2;
     const cardCenterY = rect.top + rect.height / 2;
 
@@ -47,8 +58,22 @@ function full(elem) {
     const offsetX = viewportCenterX - cardCenterX;
     const offsetY = viewportCenterY - cardCenterY;
 
-    elem.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(1.7)`;
-  } else {
-    elem.style.transform = '';
+    card.classList.add("full");
+    card.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(1.5)`;
+    setTimeout(() => card.scrollIntoView({ behavior: 'smooth', block: 'start' }), 500); 
+
+    // Shatter others 
+    cards.forEach(c => {
+      if (c !== card) {
+
+        const dx = (Math.random() - 0.5) * 1000;
+        const dy = (Math.random() - 0.5) * 1000;
+        c.style.setProperty('--x', `$(dx)px`);
+        c.style.setProperty('--y', `$(dy)px`);
+
+        c.classList.remove("reform");
+        c.classList.add("shatter");
+      }
+    });
   }
 }
