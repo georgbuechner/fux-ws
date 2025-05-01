5025cc8c7a1ee985de227cd48f857cd331489e43
fux <jan.dick@bdp.org>
2025-05-01 23:23:54 +0200

    Better full transformation

--- web/js/main.js
+++ web/js/main.js
@@ -27,10 +27,28 @@ function full(elem) {
       childDivs[ran].classList.add("shrink");
       ran = Math.floor(Math.random() * childDivs.length); 
       childDivs[ran].classList.add("shrink");
+      ran = Math.floor(Math.random() * childDivs.length); 
+      childDivs[ran].classList.add("shrink");
     } else {
       childDivs[i].classList.remove("shrink");
     }
   }
   elem.classList.remove("shrink");
+
   elem.classList.toggle("full");
+  if (elem.classList.contains("full")) {
+    const rect = elem.getBoundingClientRect();
+    const cardCenterX = rect.left + rect.width / 2;
+    const cardCenterY = rect.top + rect.height / 2;
+
+    const viewportCenterX = window.innerWidth / 2;
+    const viewportCenterY = window.innerHeight / 2;
+
+    const offsetX = viewportCenterX - cardCenterX;
+    const offsetY = viewportCenterY - cardCenterY;
+
+    elem.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(1.7)`;
+  } else {
+    elem.style.transform = '';
+  }
 }
